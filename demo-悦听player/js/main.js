163c6086e037bd72818efd76d811afaa0a5e3e0e
245Jamdedicate <245951546@qq.com>
2020-04-25 17:58:25 +0800

    第一次提交

---
+++ demo-悦听player/js/main.js
@@ -0,0 +1,103 @@
+/*
+  1:歌曲搜索接口
+    请求地址:https://autumnfish.cn/search
+    请求方法:get
+    请求参数:keywords(查询关键字)
+    响应内容:歌曲搜索结果
+
+  2:歌曲url获取接口
+    请求地址:https://autumnfish.cn/song/url
+    请求方法:get
+    请求参数:id(歌曲id)
+    响应内容:歌曲url地址
+  3.歌曲详情获取
+    请求地址:https://autumnfish.cn/song/detail
+    请求方法:get
+    请求参数:ids(歌曲id)
+    响应内容:歌曲详情(包括封面信息)
+  4.热门评论获取
+    请求地址:https://autumnfish.cn/comment/hot?type=0
+    请求方法:get
+    请求参数:id(歌曲id,地址中的type固定为0)
+    响应内容:歌曲的热门评论
+  5.mv地址获取
+    请求地址:https://autumnfish.cn/mv/url
+    请求方法:get
+    请求参数:id(mvid,为0表示没有mv)
+    响应内容:mv的地址
+*/
+
+var app= new Vue(
+  {
+    el:"#player",
+    data:{
+      query:"",
+      musicList:[],
+      musicUrl:"",
+      musicCover:"",
+      hotComments:[],
+      isPlaying:false,
+      //遮罩层的显示状态
+      isShow:false,
+      mvUrl:"",
+    },
+    methods:{
+      searchMusic:function(){
+        var that=this;
+        axios.get("https://autumnfish.cn/search?keywords="+this.query)
+        .then(function(response){
+          //console.log(response);
+          that.musicList=response.data.result.songs;
+         //console.log(that.musciclist);
+        },function(err){})
+      },
+      playMusic:function(musicId){
+          var  that=this;
+          //console.log(musicId);
+          axios.get("https://autumnfish.cn/song/url?id="+musicId).then(
+            function(response){
+              //console.log(response);
+              //console.log(response.data.data[0].url);
+              that.musicUrl=response.data.data[0].url;
+            },function(err){})
+              //歌曲详情获取.
+          axios.get("https://autumnfish.cn/song/detail?ids="+musicId).then(
+            function(response){
+              //console.log(response.data.songs[0].al.picUrl);
+              that.musicCover=response.data.songs[0].al.picUrl;
+            },function(err){}
+          )
+          //歌曲评论获取
+          axios.get("https://autumnfish.cn/comment/hot?type=0&id="+musicId).then(function(response){
+              console.log(response);
+              console.log(response.data.hotComments);
+              that.hotComments=response.data.hotComments;
+          },function(err){
+
+          })
+       },
+      play:function(){
+          this.isPlaying=true;
+       },
+      pause:function(){
+        //console.log("pause");
+        this.isPlaying=false;
+      },
+      playMv:function(mvid){
+        var that=this;
+        axios.get("https://autumnfish.cn/mv/url?id="+mvid).then(
+          function(response){
+            //console.log(response);
+            //console.log(response.data.data.url);
+            that.isShow=true;
+            that.mvUrl=response.data.data.url;
+            console.log(that.mvUrl);
+          },function(err){}
+        );
+      },
+      hide:function(){
+        this.isShow=false;
+      }
+    }
+  }
+)
